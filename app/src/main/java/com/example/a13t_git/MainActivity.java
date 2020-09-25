2f10d61d9e77a01f680f57cc28de86ec688606d5
Borossa Zoltán <b.zoltan.2001@gmail.com>
2020-09-25 14:03:35 +0200

    Ez lesz a második commit

---
+++ app/src/main/java/com/example/a13t_git/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.a13t_git;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //csak egy teszt
+    public void init(){
+
+    }
+}
